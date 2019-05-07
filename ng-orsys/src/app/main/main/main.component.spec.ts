75490a3b1f08a65c7d249148a7ecb1d79cc7ff07
Paul Narbonneau <pnarbonn@gmail.com>
2019-05-07 17:24:35 +0200

    commit final

--- ng-orsys/src/app/main/main/main.component.spec.ts
+++ ng-orsys/src/app/main/main/main.component.spec.ts
@@ -3,23 +3,36 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainComponent } from './main.component';
 
 describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+
+  let TestMod;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
-    })
-    .compileComponents();
+    
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MainComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
+  fit('should create', done => {
+    TestMod = createTestingModule({
+      declarations: [ MainComponent ]
+    });
+    console.warn(TestMod.get(MainComponent))
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
+
+const createTestingModule = config => {
+  TestBed.configureTestingModule(config).compileComponents();
+
+  return {
+    get(component){
+      const comp = TestBed.createComponent(component);
+      console.warn(123,comp)
+      comp.detectChanges();
+      return {
+        detectChanges:comp.detectChanges,
+        instance:comp.componentInstance,
+        html:comp.debugElement.nativeElement,
+      }
+    }
+  }
+}
